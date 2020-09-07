a56d74ca6956a1b6664e2dd369d63d88c797c6cf
Max Block <maxblock@pm.me>
2020-09-07 09:57:35 +0300

    update

---
+++ src/bin/time.rs
@@ -0,0 +1,12 @@
+use std::time::SystemTime;
+
+fn current_time() {
+    let now = SystemTime::now();
+    println!("{:?}", now);
+    // SystemTime { tv_sec: 1599461835, tv_nsec: 433910000 }
+}
+
+
+fn main() {
+    current_time();
+}
