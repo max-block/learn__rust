14e863d6e0079fd33b9f25035af8ce7f404056d2
Max Block <maxblock@pm.me>
2020-09-19 10:15:47 +0300

    update

--- src/bin/iterator.rs
+++ src/bin/iterator.rs
@@ -1,3 +1,11 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
+struct Data {
+    name: String,
+    value: i32,
+}
+
 pub fn map() {
     let v1: Vec<u32> = vec![1, 2, 3, 4, 5, 6, 7];
     let v2: Vec<u32> = v1.iter().map(|x| x + 1).collect();
@@ -14,7 +22,28 @@ pub fn filter() {
                           // println!("{:?}", v1); // v1 is still alive
 }
 
+pub fn filter_struct() {
+    let data = vec![
+        Data {
+            name: "n1".to_string(),
+            value: 10,
+        },
+        Data {
+            name: "n2".to_string(),
+            value: 20,
+        },
+        Data {
+            name: "n3".to_string(),
+            value: 30,
+        },
+    ];
+
+    let data: Vec<_> = data.into_iter().filter(|x| x.value >= 20).collect();
+    println!("{:#?}", data);
+}
+
 fn main() {
     // filter();
-    map();
+    // map();
+    filter_struct()
 }
