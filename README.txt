c0e2e8b1b7c75fb622adab3997a731ff699176cd
Max Block <maxblock@pm.me>
2020-10-08 14:30:24 +0300

    update

--- README.txt
+++ README.txt
@@ -3,10 +3,11 @@ NEED TO READ:
 
 
 IN PROGRESS:
-
-https://doc.rust-lang.org/stable/book/
 https://doc.rust-lang.org/stable/rust-by-example/
 https://github.com/Dhghomon/easy_rust
 
+https://youtu.be/m9lH5wIV3rQ?t=1971
+
 
 DONE:
+https://doc.rust-lang.org/stable/book/
--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let arr = [1u8, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
-    let a = &arr[2..=99];
-    println!("{:?}", a);
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Result<Vec<_>, _> = strings.into_iter().map(|s| s.parse::<i32>()).collect();
+    println!("Results: {:?}", numbers);
 }
